12c31cc6961f195df616af1baf11136ff0e7ee42
pLogicador <pedroemanuelsm0@gmail.com>
2025-04-30 17:32:40 -0300

    feat: add route api routes backend

---
+++ src/app/api/customer/route.ts
@@ -0,0 +1,33 @@
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import prismaClient from "@/lib/prisma";
+
+export async function POST(req: Request) {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    return NextResponse.json({ error: "Not authorized" }, { status: 401 });
+  }
+
+  const { name, email, phone, address, userId } = await req.json();
+
+  try {
+    await prismaClient.customer.create({
+      data: {
+        name,
+        phone,
+        email,
+        address: address ? address : "",
+        userId: userId,
+      },
+    });
+
+    return NextResponse.json({ message: "Customer registered successfully!" });
+  } catch (err) {
+    return NextResponse.json(
+      { error: "Failed create a new customer" },
+      { status: 400 }
+    );
+  }
+}
