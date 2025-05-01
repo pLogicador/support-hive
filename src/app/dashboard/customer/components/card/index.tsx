830cbae9ef6a2f444bc2288a34ed5445c0fa56ae
pLogicador <pedroemanuelsm0@gmail.com>
2025-05-01 10:10:57 -0300

    feat: add the list of registered customers

--- src/app/dashboard/customer/components/card/index.tsx
+++ src/app/dashboard/customer/components/card/index.tsx
@@ -1,14 +1,16 @@
-export function CardCustomer() {
+import { CustomerProps } from "@/utils/customer.type";
+
+export function CardCustomer({ customer }: { customer: CustomerProps }) {
   return (
     <article className="flex flex-col bg-gray-100 border-2 p-2 rounded-lg gap-2 hover:scale-105 duration-300">
       <h2>
-        <a className="font-bold">Name:</a> Name Customer
+        <a className="font-bold">Name:</a> {customer.name}
       </h2>
       <p>
-        <a className="font-bold">Email:</a> test@example.com
+        <a className="font-bold">Email:</a> {customer.email}
       </p>
       <p>
-        <a className="font-bold">Phone:</a> 11 11111 1111
+        <a className="font-bold">Phone:</a> {customer.phone}
       </p>
       <button className="self-start bg-red-500 px-4 rounded text-white mt-2 hover:bg-red-900">
         Delete
